df99c6942d6f4e126b20ab757869c6df8a84c607
Stefan Russmann <stefan.russmann@tugraz.at>
2020-05-11 14:16:36 +0200

    stefbranch added

---
+++ src/city.java
@@ -0,0 +1,13 @@
+public class city {
+
+    double x;
+    double y;
+    String city_name;
+
+    public city(double x0, double y0, String cityname){
+        x = x0;
+        y = y0;
+        city_name = cityname;
+    }
+
+}
